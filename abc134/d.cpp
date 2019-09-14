7d41bc018bf4a8a73f61769320aa6cac1399ed4f
ritosonn <ritosonn@gmail.com>
2019-09-14 22:03:21 +0900

    abc134-d

--- abc134/d.cpp
+++ abc134/d.cpp
@@ -4,5 +4,22 @@
 using namespace std;
 using ll = long long;
 int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n+1),b(n+1,0);
+    rep(i,1,1+n)cin >> a[i];
+    int cnt=0;
+    for(int i=n;i>0;--i){
+        int tmp=a[i];
+        int j=2*i;
+        while(j<=n){
+            tmp^=b[j];
+            j+=i;
+        }
+        b[i]=tmp;
+        cnt+=tmp;
+    }
+    cout << cnt << endl;
+    rep(i,1,1+n)if(b[i]>0)cout << i << " ";
     return 0;
 }
